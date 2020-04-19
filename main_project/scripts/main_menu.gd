0161770e8363afafee7dae16e15def0fd23c729e
Kirieraito <10943627+Kirieraito@users.noreply.github.com>
2020-04-20 01:08:06 +0300

    Progress on main menu

--- main_project/scripts/main_menu.gd
+++ main_project/scripts/main_menu.gd
@@ -8,7 +8,9 @@ var currentLang = Main.currentLang;
 # Параметры для загрузки локали
 func _ready():
 	# Загрузка текста кнопок в необходимой локали
-	$menuContainer/VBoxContainer/newGameButton/Label.text = Main.json_load(quotePath)[currentLang]["newGame"]
+	$menuContainer/newGameButton/Label.text = Main.json_load(quotePath)[currentLang]["newGame"]
+	$menuContainer/loadGameButton/Label.text = Main.json_load(quotePath)[currentLang]["loadGame"]
+	$menuContainer/quitButton/Label.text = Main.json_load(quotePath)[currentLang]["quit"]
 	pass # Replace with function body.
 
 
@@ -18,6 +20,16 @@ func _ready():
 
 
 func _on_newGameButton_pressed():
-	print('Button pressed!')
 	get_tree().change_scene("res://testing scene.tscn")
+	pass # Replace with function body.
+
+func _on_languageToggleBtn_pressed():
+	if (Main.currentLang == "English"):
+		Main.currentLang = "Russian"
+	else:
+		Main.currentLang = "English"
+	var fileToSave = Main.json_load("res://options/options.json")
+	fileToSave["language"] = Main.currentLang
+	Main.json_save(fileToSave, "res://options/options.json")
+	get_tree().reload_current_scene()
 	pass # Replace with function body.
